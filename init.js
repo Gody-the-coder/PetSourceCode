c8ef900afeb43c5fbfa0622fe3d3064cd3e2cdd3
Gody-the-coder <Gody-the-coder@users.noreply.github.com>
2019-01-04 08:20:34 -0500

    My first repo

---
+++ init.js
@@ -0,0 +1,17 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+  
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+       
